c9e730d2743787d9306268aa920734fc4c4915d4
unknown <shayanj@gmail.com>
2011-09-03 21:14:52 -0400

    Done with evaluating Bezier surfaces? Need to test against CPU implementation

--- simple.py
+++ simple.py
@@ -27,7 +27,6 @@ class BezierCurve:
         
         # vertices is a 2d-list [ [x0 y0 z0] [x1 y1 z1] ... [xN yN zN] ]
         self.vertices = vertices
-        
 
 # the function to read a bezier file - returns list of vertices
 def readBezierFile(fileName):
@@ -72,9 +71,9 @@ def readBezierFile(fileName):
         vertices[index-1] = newList
         
         # basic list
-        verticesL.append(float(line[0]))
-        verticesL.append(float(line[1]))
-        verticesL.append(float(line[2]))
+        #verticesL.append(float(line[0]) + 10.0)
+        #verticesL.append(float(line[1]) * 10.0)
+        #verticesL.append(float(line[2]) * 10.0)
         
         # increment
         index = index + 1
@@ -95,9 +94,10 @@ def readFile(fileName):
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-        
+    
     # try to read from a file here - returns the array
-    vertices = readBezierFile("curve1")
+    vertices = readBezierFile("curve1") * 10.0
+    print vertices
     
     # flatten the numpy array
     #vertices.shape = (4 * 3)
@@ -106,14 +106,33 @@ def main(argv=None):
     print "Vertices:" 
     print vertices.size
     
-    print "Degree: ", degreeU, " Detail: ", detail
-    
     # read the kernel file
     kernelString = readFile("sum.cl")
-    #print kernelString
     
-    # the bezier curve
-    curve1 = BezierCurve(len(vertices), 0.1, vertices)
+    # the bezier curve TODO: fix to surface instead
+    #curve1 = BezierCurve(len(vertices), 0.1, vertices)
+    
+    # Array of UV values - 36 in total (detail by 0.2)
+    uvValues = empty( (36, 2)).astype(numpy.float32)
+    print uvValues
+    index = 0
+    for u in range(0, 12, 2): # step = 2
+        for v in range(0, 12, 2):
+            # conver the ints to floats
+            fU = float(u)
+            fV = float(v)
+            uvValues[index] = [ fU/10.0, fV/10.0]
+            index = index + 1
+         
+    #uvValues = array([ [0.0, 0.0, 0.0, 0.0], [0.0, 0.2, 0.0, 0.0], [0.0, 0.4, 0.0, 0.0], [0.0, 0.6, 0.0, 0.0], [0.0, 0.8, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0],
+    #             [0.2, 0.0, 0.0, 0.0], [0.2, 0.2, 0.0, 0.0], [0.2, 0.4, 0.0, 0.0], [0.2, 0.6, 0.0, 0.0], [0.2, 0.8, 0.0, 0.0], [0.2, 1.0, 0.0, 0.0],
+    #             [0.4, 0.0, 0.0, 0.0], [0.4, 0.2, 0.0, 0.0], [0.4, 0.4, 0.0, 0.0], [0.4, 0.6, 0.0, 0.0], [0.4, 0.8, 0.0, 0.0], [0.4, 1.0, 0.0, 0.0],
+    #             [0.6, 0.0, 0.0, 0.0], [0.6, 0.2, 0.0, 0.0], [0.6, 0.4, 0.0, 0.0], [0.6, 0.6, 0.0, 0.0], [0.6, 0.8, 0.0, 0.0], [0.6, 1.0, 0.0, 0.0],
+    #             [0.8, 0.0, 0.0, 0.0], [0.8, 0.2, 0.0, 0.0], [0.8, 0.4, 0.0, 0.0], [0.8, 0.6, 0.0, 0.0], [0.8, 0.8, 0.0, 0.0], [0.8, 1.0, 0.0, 0.0],
+    #             [1.0, 0.0, 0.0, 0.0], [1.0, 0.2, 0.0, 0.0], [1.0, 0.4, 0.0, 0.0], [1.0, 0.6, 0.0, 0.0], [1.0, 0.8, 0.0, 0.0], [1.0, 1.0, 0.0, 0.0] ]);
+    print "\nUV Values: "
+    print uvValues
+    print uvValues.size
    
     ##########################
     ### OPENCL SETUP
@@ -127,16 +146,23 @@ def main(argv=None):
     mf = cl.mem_flags
     
     # create the Buffers
-    # input buffer
+    
+    # input buffers
+    # the control point vertices
     vertex_buffer = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=vertices)
     
+    # the uv buffer
+    uv_buffer = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=uvValues)
+    
     # intermediate output buffer
-    inter_buf = cl.Buffer(ctx, mf.READ_WRITE, vertices.nbytes - 32)
+    #inter_buf = cl.Buffer(ctx, mf.READ_WRITE, vertices.nbytes - 32)
+    
+    # create intermediate local buffer
+    #local_buf
     
     # final output
-    output_buf = cl.Buffer(ctx, mf.WRITE_ONLY, 32) # just one value
-    
-    
+    #output_buf = cl.Buffer(ctx, mf.WRITE_ONLY, 32) # just one value
+    output_buffer = cl.Buffer(ctx, mf.WRITE_ONLY, uvValues.nbytes * 2) # double the amount of value
     
     # the OpenCL program
     prg = cl.Program(ctx, kernelString).build()
@@ -144,31 +170,54 @@ def main(argv=None):
     # timer start
     start = time.time()
     
-    # Loop through all possible u-values
-    result = []
+    # the global size (number of uv-values): 36 work-items
+    globalSize = 36 * 9
+
+    localSize = 9 # for bicubic patches
+    
+    # evaluate
+    prg.bezierEval(cq, (globalSize,), (localSize,), vertex_buffer, uv_buffer, output_buffer, cl.LocalMemory(9 * 9), cl.LocalMemory(9 * 4)) # local_buffer)#numpy.int32(degreeU), numpy.float32(u), vertex_buffer, inter_buf)
+    
+    # read back the result - works!
+    #eval = empty((36, 4)).astype(numpy.float32);
+    eval = empty( 36*4).astype(numpy.float32)#numpy.empty_like(vertices)
+    cl.enqueue_read_buffer(cq, output_buffer, eval).wait()
+    
+    
+    ####################
+    #### NAIVE METHOD ########
+    #### Loop through all possible u-values - naive method
+    ''' result = []
     u = 0.0
-    while u < 1.0:
+    v = 0.0
+    while u <= 1.0:
         # call the program
         # NOTE: Need to convert to proper formats using numpy
         prg.bezier(cq, ((degreeU+1),), None, numpy.int32(degreeU), numpy.float32(u), vertex_buffer, inter_buf)
         
-        # call again
-        prg.bezier(cq, (1,), None, numpy.int32(degreeU), numpy.float32(u), inter_buf, output_buf)
+        v = 0.0
         
-        # get the result back
-        eval = empty( 4).astype(numpy.float32)#numpy.empty_like(vertices)
-        cl.enqueue_read_buffer(cq, output_buf, eval).wait()
-        
-        # add it to results
-        result.append(eval)
+        while v <= 1.0:
+            # call again
+            prg.bezier(cq, (1,), None, numpy.int32(degreeU), numpy.float32(v), inter_buf, output_buf)
+            
+            # get the result back
+            eval = empty( 4).astype(numpy.float32)#numpy.empty_like(vertices)
+            cl.enqueue_read_buffer(cq, output_buf, eval).wait()
+            
+            # add it to results
+            result.append(eval)
+            
+            v += 0.5
         
         # increment
-        u += 0.1
+        u += 0.5 '''
         
     print "It took: ", time.time() - start,"seconds."
-        
-    print "\nResult:"
-    print result
+    print eval
+    #print "\nResult:"
+    #print result
+    ########### END OF NAIVE METHOD ###############
     
 # main invocation
 if __name__ == "__main__":
--- sum.cl
+++ sum.cl
@@ -2,7 +2,7 @@ __kernel void bezier(int degree, float detail,
 					 __global const float4 *a, __global float4 *c)
 					 //__local float As[16])
 {
-	int gid = get_global_id(0); // row number?
+	int gid = get_global_id(0)/9; // row number?
 	// assume stride = (degree)
 	// tensor-product patch of equal degrees
 	int aid = gid * ((degree+1));
@@ -21,5 +21,105 @@ __kernel void bezier(int degree, float detail,
 				 detail * detail * point3; 
 	
 	c[gid] = val;
-	
 }
+
+// Linear interpolation between a pair of points by a u-value
+float4 lerp(float4 val1, float4 val2, float u) {
+	return (1.0 - u) * val1 + u * val2;
+}
+
+// The optimized version:
+// 15th August 2011
+//__kernel __attribute__
+//(( reqd_work_group_size (9, 1, 1)))
+__kernel void bezierEval(__global const float4 *controlPoints, __global float2 *uvValues,
+                        __global float4 *output, __local float4 *local_points, __local float4 *local_points2) {
+ 
+ 	// get the global id and then just output that
+ 	int gid = get_global_id(0)/9;
+ 	
+ 	// get the uv values - PROBLEM: NOT READING UV VALUES
+ 	float2 uv = uvValues[gid];//get_global_id(0)];
+ 	
+ 	// get the row
+ 	int lid = get_local_id(0);
+ 	int row = lid/3; 				// 3 = degree
+ 	
+ 	// get the 4 control points you'll need
+ 	/****************
+ 	  b10--b11
+ 	   |   |
+ 	  b00--b01
+ 	****************/
+ 	
+ 	float4 b00 = controlPoints[row + lid];
+ 	float4 b01 = controlPoints[row + lid+1];
+ 	float4 b10 = controlPoints[row + lid+4];
+ 	float4 b11 = controlPoints[row + lid+5];
+ 	
+ 	// do linear interpolation across u first
+ 	float4 val1 = lerp(b00, b01, uv.x);
+ 	float4 val2 = lerp(b10, b11, uv.x);
+ 	
+ 	// then across v
+ 	float4 newPoint = lerp(val1, val2, uv.y);
+ 	
+ 	// store it in the local memory
+ 	local_points[lid] = newPoint;
+ 	
+ 	// TEMPORARY TEST: Try to just put the lid in there.
+ 	/* local_points[lid] = (float4)(lid, lid, lid, lid);
+ 	local_points[lid].s0 = (float)lid;
+ 	local_points[lid].s1 = (float)lid;
+ 	local_points[lid].s2 = (float)lid;
+ 	local_points[lid].s3 = (float)lid;*/
+ 	
+ 	// synchronize in work-group
+ 	barrier(CLK_LOCAL_MEM_FENCE);
+ 	
+ 	// only do it for certain work-items
+ 	/*if (lid == 0) {
+ 			output[gid] =   local_points[0] + 
+ 					   local_points[1] + 
+ 					   local_points[2] + 
+ 					   local_points[3] + 
+ 					   local_points[4] + 
+ 					   local_points[5] + 
+ 					   local_points[6] + 
+ 					   local_points[7] + 
+ 					   local_points[8]; 
+ 	}*/
+ 	if (lid < 4) {
+ 		row = lid/2; 		// 2 = degree
+ 		float4 b002 = local_points[row + lid];
+ 		float4 b012 = local_points[row + lid+1];
+ 		float4 b102 = local_points[row + lid+3];
+ 		float4 b112 = local_points[row + lid+4];
+ 		
+ 		// do linear interpolation across u first
+ 		float4 val12 = lerp(b002, b012, uv.x);
+ 		float4 val22 = lerp(b102, b112, uv.x);
+ 	
+ 		// then across v
+ 		float4 newPoint2 = lerp(val12, val22, uv.y);
+ 		
+ 		local_points2[lid] = newPoint2;
+ 		barrier(CLK_LOCAL_MEM_FENCE);
+ 		
+ 		// only do it for the first work-item
+ 		if (lid == 0) {
+ 			float4 val13 = lerp(local_points2[0], local_points2[1], uv.x);
+ 			float4 val23 = lerp(local_points2[2], local_points2[3], uv.x);
+ 			
+ 			float4 newPoint3 = lerp(val13, val23, uv.y);
+ 			output[gid] = newPoint3;//(float4)(uv.s0, uv.s1, 0.0, 0.0);//newPoint3;
+ 		}
+ 	} 
+ 	
+ 	//output[gid] = b00;//controlPoints[gid];
+ 	
+ 	/* output[gid].s0 = gid;
+ 	output[gid].s1 = gid;
+ 	output[gid].s2 = gid;
+ 	output[gid].s3 = gid; */
+ }
